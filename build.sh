13975813cac0a819ea615c67136ea3599586ace2
Hetsh <git@hetsh.de>
2020-11-14 13:21:36 +0100

    Only upload current tag, improved documentation

--- build.sh
+++ build.sh
@@ -22,10 +22,10 @@ fi
 # Build the image
 APP_NAME="starbound"
 IMG_NAME="hetsh/$APP_NAME"
-docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
+docker build --tag "$IMG_NAME:latest" --tag "$IMG_NAME:$_NEXT_VERSION" .
 
-# Start the test
 case "${1-}" in
+	# Test with default configuration
 	"--test")
 		# Set up temporary directory
 		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
@@ -49,9 +49,11 @@ case "${1-}" in
 		--name "$APP_NAME" \
 		"$IMG_NAME"
 	;;
+	# Push image to docker hub
 	"--upload")
 		if ! tag_exists "$IMG_NAME"; then
-			docker push "$IMG_NAME"
+			docker push "$IMG_NAME:latest"
+			docker push "$IMG_NAME:$_NEXT_VERSION"
 		fi
 	;;
 esac
